033e2757d8e0fa302fbbc4a2a811d7e0dac54c6b
Mike Garrahan <mgarraha@users.noreply.github.com>
2019-01-22 14:00:42 -0500

    goto elimination

--- text.f90
+++ text.f90
@@ -14,28 +14,30 @@ contains
 !  PRINT THE MESSAGE WHICH STARTS AT LINES(N).  PRECEDE IT WITH A BLANK LINE
 !  UNLESS BLKLIN IS FALSE.
 
-      INTEGER N
-      INTEGER I,K,L,M
+      integer, intent(in) :: N
+      integer :: I, K, L, M
       character(len=80) :: BUF
       LOGICAL BLKLIN
       COMMON /BLKCOM/ BLKLIN
 
-      if (N == 0) RETURN
-      if (LINES(N+1) == IA5('>$<  ')) RETURN
-      if (BLKLIN) PRINT 2
+      if (N == 0) return
+      if (LINES(N+1) == IA5('>$<  ')) return
+      if (BLKLIN) print 2
       K=N
-1     L=ABS(LINES(K))-1
-      K=K+1
-      BUF=''
-      do I=K,L
-         M=5*(I-K)
-         BUF(M+1:M+5)=A5I(LINES(I))
+      do
+         L = abs(LINES(K)) - 1
+         K = K + 1
+         BUF = ''
+         do I = K, L
+            M = 5 * (I - K)
+            BUF(M+1:M+5) = A5I(LINES(I))
+         end do
+         print 2, BUF
+2        format (' ', A)
+         K=L+1
+         if (LINES(K) < 0) exit
       end do
-      PRINT 2,BUF
-2     FORMAT(' ',A)
-      K=L+1
-      if (LINES(K) >= 0) GOTO 1
-      RETURN
+      return
    end subroutine SPEAK
 
 
@@ -43,10 +45,10 @@ contains
 
 !  PRINT THE I-TH "RANDOM" MESSAGE (SECTION 6 OF DATABASE).
 
-      INTEGER I
+      integer, intent(in) :: I
 
-      if (I /= 0) CALL SPEAK(RTEXT(I))
-      RETURN
+      if (I /= 0) call SPEAK(RTEXT(I))
+      return
    end subroutine RSPEAK
 
 
@@ -54,35 +56,38 @@ contains
 
 !  CALL YESX (BELOW) WITH MESSAGES FROM SECTION 6.
 
-      INTEGER X,Y,Z
+      integer, intent(in) :: X, Y, Z
 
-      YES=YESX(X,Y,Z,RSPEAK)
-      RETURN
+      YES = YESX(X, Y, Z, RSPEAK)
+      return
    end function YES
 
 
-   logical function YESX(X,Y,Z,SPK)
+   logical function YESX(X, Y, Z, SPK)
 
 !  PRINT MESSAGE X, WAIT FOR YES/NO ANSWER.  IF YES, PRINT Y AND LEAVE YEA
 !  TRUE; IF NO, PRINT Z AND LEAVE YEA FALSE.  SPK IS EITHER RSPEAK OR MSPEAK.
 
-      INTEGER X,Y,Z
+      integer, intent(in) :: X, Y, Z
       integer(kind=A5) :: REPLY, JUNK1, JUNK2, JUNK3
-      EXTERNAL SPK
+      external :: SPK
 
-1     if (X /= 0) CALL SPK(X)
-      CALL GETIN(REPLY,JUNK1,JUNK2,JUNK3)
-      if (REPLY == IA5('YES  ') .or. REPLY == IA5('Y    ')) GOTO 10
-      if (REPLY == IA5('NO   ') .or. REPLY == IA5('N    ')) GOTO 20
-      PRINT 9
-9     FORMAT(/' PLEASE ANSWER THE QUESTION.')
-      GOTO 1
-10    YESX=.true.
-      if (Y /= 0) CALL SPK(Y)
-      RETURN
-20    YESX=.false.
-      if (Z /= 0) CALL SPK(Z)
-      RETURN
+      do
+         if (X /= 0) call SPK(X)
+         call GETIN(REPLY, JUNK1, JUNK2, JUNK3)
+         if (REPLY == IA5('YES  ') .or. REPLY == IA5('Y    ')) then
+            YESX = .true.
+            if (Y /= 0) call SPK(Y)
+            return
+         else if (REPLY == IA5('NO   ') .or. REPLY == IA5('N    ')) then
+            YESX = .false.
+            if (Z /= 0) call SPK(Z)
+            return
+         else
+            print 9
+9           format (/' PLEASE ANSWER THE QUESTION.')
+         end if
+      end do
    end function YESX
 
 
@@ -105,55 +110,57 @@ contains
       DATA MASKS/O"4000000000",O"20000000",O"100000",O"400",O"2",0/  &
            ,BLANKS/O"201004020100"/
 
-      if (BLKLIN) PRINT 1
-1     FORMAT()
-2     READ 3,ITXT
-3     FORMAT(A20)
-      do I=1,4
-         IWORD=ITXT(5*I-4:5*I)
-         A(I)=IA5(IWORD)
-      end do
-      J=0
-      do I=1,4
-         if (A(I) /= BLANKS)J=1
-         ! convert lowercase to uppercase
-         A(I)=IAND(A(I),IEOR(ISHFT(IAND(A(I),IA5('@@@@@')),-1),-1))
-      end do
-      if (BLKLIN .and. J == 0) GOTO 2
-
-      SECOND=0
-      WORD1=A(1)
-      WORD1X=A(2)
-      WORD2=0
-
-      do J=1,4
-         do K=1,5
-            ! is char K a blank?
-            if (ibits(A(J), 36 - 7 * K, 7) /= iachar(' ')) then
-               if (SECOND == 3) GOTO 20
-               if (SECOND /= 1) cycle
-               MSK=-MASKS(6-K)
-               MSK2=-2-MSK
-               MSK=IAND(MSK,O"777777777776")
-               WORD2=IAND(ISHFT(A(J),7*(K-1)),MSK)  &
-                     +IAND(ISHFT(A(J+1),7*(K-6)),(MSK2))
-               WORD2X=IAND(ISHFT(A(J+1),7*(K-1)),MSK)  &
-                     +IAND(ISHFT(A(J+2),7*(K-6)),(MSK2))
-               SECOND=2
-               cycle
-            end if
-            if (SECOND == 2) SECOND=3
-            if (SECOND /= 0) cycle
-            SECOND=1
-            ! overwrite WORD1(K+1:) with blanks
-            if (J == 1) call mvbits(BLANKS, 0, 36 - 7 * K, WORD1, 0)
+      if (BLKLIN) print 1
+1     format ()
+      retry: do
+         read 3, ITXT
+3        format (A20)
+         do I = 1, 4
+            IWORD = ITXT(5*I-4:5*I)
+            A(I) = IA5(IWORD)
          end do
-      end do
-      RETURN
+         J = 0
+         do I = 1, 4
+            if (A(I) /= BLANKS) J = 1
+            ! convert lowercase to uppercase
+            A(I) = iand(A(I), ieor(ishft(iand(A(I), IA5('@@@@@')), -1), -1))
+         end do
+         if (BLKLIN .and. J == 0) cycle retry
 
-20    PRINT 21
-21    FORMAT(/' PLEASE STICK TO 1- AND 2-WORD COMMANDS.'/)
-      GOTO 2
+         SECOND = 0
+         WORD1 = A(1)
+         WORD1X = A(2)
+         WORD2 = 0
+
+         do J = 1, 4
+            do K = 1, 5
+               ! is char K a blank?
+               if (ibits(A(J), 36 - 7 * K, 7) /= iachar(' ')) then
+                  if (SECOND == 3) then
+                     print 21
+21                   format (/' PLEASE STICK TO 1- AND 2-WORD COMMANDS.'/)
+                     cycle retry
+                  end if
+                  if (SECOND /= 1) cycle
+                  MSK = -MASKS(6-K)
+                  MSK2 = -2 - MSK
+                  MSK = iand(MSK, O"777777777776")
+                  WORD2 = iand(ishft(A(J), 7*(K-1)), MSK)  &
+                        + iand(ishft(A(J+1), 7*(K-6)), MSK2)
+                  WORD2X = iand(ishft(A(J+1), 7*(K-1)), MSK)  &
+                        + iand(ishft(A(J+2), 7*(K-6)), MSK2)
+                  SECOND = 2
+                  cycle
+               end if
+               if (SECOND == 2) SECOND = 3
+               if (SECOND /= 0) cycle
+               SECOND = 1
+               ! overwrite WORD1(K+1:) with blanks
+               if (J == 1) call mvbits(BLANKS, 0, 36 - 7 * K, WORD1, 0)
+            end do
+         end do
+         return
+      end do retry
    end subroutine GETIN
 
 
